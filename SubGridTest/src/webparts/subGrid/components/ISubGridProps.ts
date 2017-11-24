0c675af9dc42cbc0b90d4184790efbf9e8742a7e
Little <raymond.little@company-net.com>
2017-11-24 08:30:25 +0000

    adding intial version

---
+++ SubGridTest/src/webparts/subGrid/components/ISubGridProps.ts
@@ -0,0 +1,3 @@
+export interface ISubGridProps {
+  description: string;
+}
---
+++ SubGridTest/src/webparts/subGrid/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISubGridWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SubGridWebPartStrings' {
+  const strings: ISubGridWebPartStrings;
+  export = strings;
+}
